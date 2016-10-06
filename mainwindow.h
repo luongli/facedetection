062997ecdc7d4f74abec2741e455481e5835d495
LuongNV <luongnv@ows.edu.vn>
2016-10-06 11:44:30 +0700

    Add: show camera on GUI

--- mainwindow.h
+++ mainwindow.h
@@ -5,10 +5,24 @@
 #include <iostream>
 #include <sstream>
 #include <fstream>
+#include <QObject>
+#include <stdio.h>
+#include <stdlib.h>
 
+// opencv lib
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
+#include "opencv2/opencv.hpp"
+#include "opencv2/videoio.hpp"
+#include "opencv2/video/background_segm.hpp"
+#include <cv.h>
+#include <highgui.h>
+
+#include "my_qlabel.h"
+
+using namespace cv;
+using namespace std;
 
 namespace Ui {
 class MainWindow;
@@ -20,8 +34,11 @@ class MainWindow : public QMainWindow
 
 public:
     explicit MainWindow(QWidget *parent = 0);
+    void setImage(Mat img, my_qlabel *label);
     ~MainWindow();
 
+public slots:
+    void openCamera();
 private:
     Ui::MainWindow *ui;
 };
